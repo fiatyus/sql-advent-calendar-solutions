13821f81c40d5ca3dbd56de3b977dc3849f8690c
Afia Tyus <162537013+fiatyus@users.noreply.github.com>
2026-01-04 21:20:37 -0500

    Adding challenge solutions

---
+++ day-06.sql
@@ -0,0 +1,24 @@
+-- SQL Advent Calendar - Day 6
+-- Title: Ski Resort Snowfall Rankings
+-- Difficulty: hard
+--
+-- Question:
+-- Buddy is planning a winter getaway and wants to rank ski resorts by annual snowfall. Can you help him bucket these ski resorts into quartiles?
+--
+-- Buddy is planning a winter getaway and wants to rank ski resorts by annual snowfall. Can you help him bucket these ski resorts into quartiles?
+--
+
+-- Table Schema:
+-- Table: resort_monthly_snowfall
+--   resort_id: INT
+--   resort_name: VARCHAR
+--   snow_month: INT
+--   snowfall_inches: DECIMAL
+--
+
+-- My Solution:
+
+select resort_id, resort_name, sum(snowfall_inches),
+  NTILE(4) OVER (ORDER BY sum(snowfall_inches)) AS quartile
+from resort_monthly_snowfall
+group by resort_id
